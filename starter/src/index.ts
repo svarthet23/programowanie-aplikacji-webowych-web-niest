9cd4eae5f009bccb71b0f6ecadd4b751a3ce6112
Podgórski Oskar <oskar.podgorski@microsoft.wsei.edu.pl>
2020-03-08 12:27:34 +0100

    Created Project

---
+++ starter/src/index.ts
@@ -0,0 +1,4 @@
+var input0 = document.getElementById('input0').textContent
+var input1 = document.getElementById('input1').textContent
+var input2 = document.getElementById('input2').textContent
+var input3 = document.getElementById('input3').textContent
