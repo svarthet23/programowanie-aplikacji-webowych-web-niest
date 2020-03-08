aa85c2ab56ae2cb7144db9c01b2b83dbe477ca41
Podgórski Oskar <oskar.podgorski@microsoft.wsei.edu.pl>
2020-03-08 12:50:40 +0100

    npx tsc

---
+++ starter/src/index.js
@@ -0,0 +1,9 @@
+document.getElementById("input0").addEventListener("mousemove", function () {
+    alert("test");
+    document.getElementById("test").textContent = "working";
+});
+var input0 = document.getElementById('input0').textContent;
+var input1 = document.getElementById('input1').textContent;
+var input2 = document.getElementById('input2').textContent;
+var input3 = document.getElementById('input3').textContent;
+//# sourceMappingURL=index.js.map
--- starter/src/index.ts
+++ starter/src/index.ts
@@ -1,3 +1,10 @@
+
+document.getElementById("input0").addEventListener("mousemove", function(){
+    alert("test");
+    document.getElementById("test").textContent = "working";
+});
+
+
 var input0 = document.getElementById('input0').textContent
 var input1 = document.getElementById('input1').textContent
 var input2 = document.getElementById('input2').textContent
